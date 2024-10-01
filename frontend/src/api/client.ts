ba87e45c96335a54b10608c48ddc3f2b2e489cd0
Archit Gupta <rarchitgupta@gmail.com>
2024-10-01 18:15:47 -0400

    Endpoint for deployment

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://localhost:8000" });
+const client = axios.create({ baseURL: "https://wolfjobs.onrender.com" });
 
 export default client;
--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -1,4 +1,4 @@
-const API_ROOT = "http://localhost:8000/api/v1";
+const API_ROOT = "https://wolfjobs.onrender.com/api/v1";
 
 export const loginURL = `${API_ROOT}/users/create-session`;
 export const signupURL = `${API_ROOT}/users/signup`;
